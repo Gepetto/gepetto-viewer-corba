8e8b9cfed4252e00a1b67e0fbd843d24bc43263e
Pierre Fernbach <pierre.fernbach@laas.fr>
2015-05-21 18:06:12 +0200

    move node-rod in gepetto viewer, and add python method : addRod

--- src/graphical-interface.impl.hh
+++ src/graphical-interface.impl.hh
@@ -58,6 +58,8 @@ public:
 
   virtual bool addCapsule(const char* capsuleName, float radius, float height, const value_type* color) throw (Error);
 
+  virtual bool addRod (const char* rodNameCorba, const value_type* colorCorba, const float radius, const float length,short maxCapsule) throw (Error);
+
   virtual bool resizeCapsule(const char* capsuleNameCorba, float newHeight) throw(Error);
 
   virtual bool addMesh(const char* meshNameCorba, const char* meshPathCorba)  throw (Error);
