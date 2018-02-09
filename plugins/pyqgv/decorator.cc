f29a7c33fc8f519cb09c84ea134eafbf28c8b4e6
Joseph Mirabel <jmirabel@laas.fr>
2018-02-09 11:07:04 +0100

    Add pyqgv plugin.

---
+++ plugins/pyqgv/decorator.cc
@@ -0,0 +1,54 @@
+// Copyright (c) 2018, Joseph Mirabel
+// Authors: Joseph Mirabel (joseph.mirabel@laas.fr)
+//
+// This file is part of gepetto-viewer-corba.
+// gepetto-viewer-corba is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// gepetto-viewer-corba is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// gepetto-viewer-corba. If not, see <http://www.gnu.org/licenses/>.
+
+#include <decorator.hh>
+
+#include <PythonQt/PythonQt.h>
+#include <QGVScene.h>
+
+// ------- QGVScene ------------------------------------------- //
+QGVScene* QGVDecorator::new_QGVScene(const QString &name) { return new QGVScene(name); }
+QGVScene* QGVDecorator::new_QGVScene(const QString &name, QObject *parent) { return new QGVScene(name, parent); }
+void QGVDecorator::delete_QGVScene(QGVScene* s) { delete s; }
+
+void QGVDecorator::setGraphAttribute(QGVScene* s, const QString &name, const QString &value) { s->setGraphAttribute(name, value); }
+void QGVDecorator::setNodeAttribute (QGVScene* s, const QString &name, const QString &value) { s->setNodeAttribute (name, value); }
+void QGVDecorator::setEdgeAttribute (QGVScene* s, const QString &name, const QString &value) { s->setEdgeAttribute (name, value); }
+
+QGVNode* QGVDecorator::addNode(QGVScene* s, const QString& label) { return s->addNode (label); }
+QGVEdge* QGVDecorator::addEdge(QGVScene* s, QGVNode* source, QGVNode* target) { return s->addEdge (source, target); }
+QGVEdge* QGVDecorator::addEdge(QGVScene* s, QGVNode* source, QGVNode* target, const QString& label) { return s->addEdge (source, target, label); }
+// QGVSubGraph* QGVDecorator::addSubGraph(const QString& name, bool cluster=true);
+
+void QGVDecorator::setRootNode(QGVScene* s, QGVNode *node) { s->setRootNode (node); }
+
+// void QGVDecorator::setNodePositionAttribute();
+
+void QGVDecorator::loadLayout (QGVScene* s, const QString &text)                          { s->loadLayout(text); }
+void QGVDecorator::applyLayout(QGVScene* s, const QString &algorithm)                     { s->applyLayout(algorithm); }
+void QGVDecorator::render     (QGVScene* s, const QString &algorithm)                     { s->render(algorithm); }
+void QGVDecorator::render     (QGVScene* s, const QString &algorithm, const QString file) { s->render(algorithm, file); }
+void QGVDecorator::freeLayout (QGVScene* s) { s->freeLayout(); }
+void QGVDecorator::clear      (QGVScene* s) { s->clear(); }
+// ------- QGVScene ------------------------------------------- //
+
+void registerQGV ()
+{
+  PythonQt::self()->addDecorators (new QGVDecorator ());
+  PythonQt::self()->registerCPPClass ("QGVScene", "QGraphicsScene", "qgv");
+  PythonQt::self()->registerCPPClass ("QGVNode" , "QGraphicsItem" , "qgv");
+  PythonQt::self()->registerCPPClass ("QGVEdge" , "QGraphicsItem" , "qgv");
+}
---
+++ plugins/pyqgv/decorator.hh
@@ -0,0 +1,58 @@
+// Copyright (c) 2018, Joseph Mirabel
+// Authors: Joseph Mirabel (joseph.mirabel@laas.fr)
+//
+// This file is part of gepetto-viewer-corba.
+// gepetto-viewer-corba is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// gepetto-viewer-corba is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// gepetto-viewer-corba. If not, see <http://www.gnu.org/licenses/>.
+
+#include "qgv.h"
+#include <QObject>
+
+class QGVScene;
+class QGVNode;
+class QGVEdge;
+
+class QGVCORE_EXPORT QGVDecorator : public QObject
+{
+    Q_OBJECT
+
+public slots:
+
+// ------- QGVScene ------------------------------------------- //
+    QGVScene* new_QGVScene(const QString &name);
+    QGVScene* new_QGVScene(const QString &name, QObject *parent);
+    void delete_QGVScene(QGVScene* s);
+
+    void setGraphAttribute(QGVScene* s, const QString &name, const QString &value);
+    void setNodeAttribute (QGVScene* s, const QString &name, const QString &value);
+    void setEdgeAttribute (QGVScene* s, const QString &name, const QString &value);
+
+    QGVNode* addNode(QGVScene* s, const QString& label);
+    QGVEdge* addEdge(QGVScene* s, QGVNode* source, QGVNode* target);
+    QGVEdge* addEdge(QGVScene* s, QGVNode* source, QGVNode* target, const QString& label);
+    // QGVSubGraph* addSubGraph(const QString& name, bool cluster=true);
+
+    void setRootNode(QGVScene* s, QGVNode *node);
+
+    // void setNodePositionAttribute();
+
+    void loadLayout (QGVScene* s, const QString &text);
+    void applyLayout(QGVScene* s, const QString &algorithm);
+    void render     (QGVScene* s, const QString &algorithm);
+    void render     (QGVScene* s, const QString &algorithm, const QString file);
+    void freeLayout (QGVScene* s);
+    void clear      (QGVScene* s);
+// ------- QGVScene ------------------------------------------- //
+
+};
+
+void registerQGV ();
