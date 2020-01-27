66debc82c3bd46e96976af125710842b37ae7ed6
Joseph Mirabel <jmirabel@laas.fr>
2020-01-27 10:33:37 +0100

    Fix documentation.

--- doc/main.hh
+++ doc/main.hh
@@ -1,80 +1,23 @@
 /**
 
-\mainpage Gepetto Viewer
+\mainpage CORBA interface of Gepetto Viewer
 
-This package provides a graphical user interface to visualize robots. There are
-two compatible interfaces to interact with the interface.
-\li the CORBA interface with Python and C++ clients.
-\li the graphical user interface.
+This package implements a CORBA interface to \ref gepetto_viewer_index_html "gepetto-viewer".
+Refers to its doc for further details on the interface itself.
 
-To start the interface, use
-\code{bash} gepetto-gui \endcode
+\par Basic usage
 
-To get some help on the command line options, use
-\code{bash} gepetto-gui --help \endcode
+The IDL interface of the viewer is defined by gepetto::corbaserver::GraphicalInterface.
 
-To get command line completion for `gepetto-gui` command, see package
-https://github.com/humanoid-path-planner/hpp-tools.
-
-\section gepetto_gui_corba CORBA interface
-
-The IDL interface of the server is defined by gepetto::corbaserver::GraphicalInterface.
-
+To create a client to the interface, you may use the following code.
 \code{py}
-from gepetto.corbaserver import Client
-client = Client ()
-# open a new window
-client.gui.createWindow ("w")
+from gepetto.corbaserver import gui_client
+# Create a client to the GUI, a window name "w" and a group node "w".
+# dont_raise if the GUI is not available (in which case gui will be None).
+gui = gui_client(windowName = "w", dont_raise=True)
 # refer to the idl interface to control gepetto-viewer.
 \endcode
 
-\section gepetto_gui_ui Graphical user interface
-
-\par Configuration files
-
-The interface can be customized using a few setting files.
-See gepetto::gui::Settings::readSettingFile for more details on configuration files.
-
-To generate a new configuration use:
-\code{bash} gepetto-gui -c newConfigName -g \endcode
-
-To use the configuration *config*:
-\code{bash} gepetto-gui -c newConfigName \endcode
-
-\par Extending the interface
-
-There are two ways of extending the interface: Python and C++. Python
-plugins are easier to write than C++ plugins, but they do not offer
-full control of the interface (yet).
-
-See the \ref pluginlist for some examples.
-Building and installing plugins is made easy with \ref gepetto_viewer_corba_cmake_macros.
-
-- *Python plugins*
-  See the documentation of gepetto::gui::PythonWidget for instructions
-  and examples of Python plugin usage.
-
-  The Python console is a good tool for debugging. The following variables
-  are accessible:
-  + \c mainWindow: gepetto::gui::MainWindow
-  + \c osg: the widget responsible for the first created window (class gepetto::gui::OSGWidget)
-
-- *C++ Plugin*
-  See documentation of gepetto::gui::PluginInterface,  for details on how to write a plugin.
-
-\defgroup pluginlist List of available plugins
-
-This is the list of available plugins. See each plugin to know how to activate it.
-
-\defgroup plugin Plugin interfaces
-\{
-Interface of C++ and Python plugins.
-
-  \defgroup plugin_cpp C++ Plugin API
-  Descriptions of the available interfaces.
-
-  \defgroup plugin_python Python plugin API
-  These slots are available for Python scripting in plugins.
-\}
+Extra features can be found in gepetto.corbaserver.tools.
 
 */
